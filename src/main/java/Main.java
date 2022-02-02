a4160e8cd494d44704dad2f54f23c74e901b332c
Eugeniu Berber <eugeniuberber@gmail.com>
2022-02-02 11:24:38 -0700

    second commit with changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         for (int i = 0 ; i < 10 ; i++) {
             System.out.println("TEST "+i);
+            System.out.println("Changes");
             Thread.sleep(2000);
         }
     }
